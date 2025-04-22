8180a2354d7a4e1729d45a6fcb77af9544068406
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-04-22 12:16:00 +0545

    folder structuring

--- js/scripts/10-rock-paper.js
+++ js/scripts/10-rock-paper.js
@@ -52,8 +52,8 @@ function playGame(playerMove) {
   document.querySelector('.js-result').innerHTML = ` 
 
   You 
-  <img src="images/${playerMove}-emoji.png" class="move-icon">
-  <img src="images/${computerMove}-emoji.png" class="move-icon">
+  <img src="../images/${playerMove}-emoji.png" class="move-icon">
+  <img src="../images/${computerMove}-emoji.png" class="move-icon">
  Computer`;
 }
 
