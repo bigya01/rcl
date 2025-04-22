b659bb03d7c8663658c3176eb914b292e3bbc315
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-04-22 15:44:27 +0545

    9 assignments

---
+++ js/assignments/scripts/10ab.js
@@ -0,0 +1,2 @@
+let test= document.querySelector('.js-button');
+console.log(test.classList.contains('js-button'));
---
+++ js/assignments/scripts/10def.js
@@ -0,0 +1,14 @@
+
+
+function display(buttonElement){
+   
+    if(!buttonElement.classList.contains('is-toggled')){
+       buttonElement.classList.add('is-toggled');
+    }
+    else{
+        buttonElement.classList.remove('is-toggled');
+    }
+}
+
+
+
