8fe3c9c7c728ac782f2e2b7f5a0f9d4d64bbebcb
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-04-22 11:59:06 +0545

    filter & map

--- js/scripts/12-todo-foreach.js
+++ js/scripts/12-todo-foreach.js
@@ -1,10 +1,8 @@
 const todolist = [];
 
-document.querySelector(".add-todo-button").addEventListener("click", ()=>{
+document.querySelector(".add-todo-button").addEventListener("click", () => {
   addTodo();
 });
-
-
 
 function addTodo() {
   let taskInputElement = document.querySelector(".todo-list");
@@ -23,21 +21,20 @@ function addTodo() {
 function display() {
   let displayList = document.querySelector(".display-list");
   displayList.innerHTML = "";
-
-
-
   todolist.forEach((todoObject, index) => {
     const { task, dueDate } = todoObject;
-    const listt = `<div>${task}</div>
+    const listt = `
+        <div>${task}</div>
         <div>${dueDate}</div>
-        <button class="delete-btn" onclick="
-        todolist.splice(${index},1);
-        display();
-        ">Delete</button>`;
+        <button class="delete-btn">Delete</button>`;
 
     displayList.innerHTML += listt;
   });
 
- 
-  document.querySelectorAll
+  document.querySelectorAll(".delete-btn").forEach((deleteButton, index) => {
+    deleteButton.addEventListener("click", () => {
+      todolist.splice(index, 1);
+      display();
+    });
+  });
 }
