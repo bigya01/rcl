be9eaca2e4ccb0d3ee29a37e799997c92e77747e
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-04-23 17:14:41 +0545

    delete products from HTML

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -13,7 +13,9 @@ cart.forEach((cartItem) => {
     }
   });
 
-  cartSummaryHTML += `<div class="cart-item-container">
+  cartSummaryHTML += `<div class="cart-item-container 
+            js-cart-item-container-${matchingProduct.id}
+            ">
             <div class="delivery-date">
               Delivery date: Wednesday, June 15
             </div>
@@ -96,6 +98,8 @@ document.querySelectorAll(".js-delete-link").forEach((deleteLink) => {
     deleteLink.addEventListener("click",()=>{
         const deletedProductId=deleteLink.dataset.productId;
         removeFromCart(deletedProductId);
+
+        document.querySelector(`.js-cart-item-container-${deletedProductId}`).remove();
     })
 
 });
