9252d739483e939b6d0fac824113a470670699aa
bigya01 <078bct032.bindu@pcampus.edu.np>
2025-04-30 12:49:13 +0545

    promises to async await

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -1,29 +1,38 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import { loadProducts,loadProductsFetch } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import '../data/cart-class.js'
 // import '../data/backend-practice.js'
 
 
-Promise.all([
-    loadProductsFetch(),
-    new Promise((resolve) => {
-      loadCart(() => {
-        resolve();
-      });
-    })
-  ]).then(() => {
-      
-          renderOrderSummary();
-          renderPaymentSummary();
-        
-  })
+// using promises
+// Promise.all([
+//   loadProductsFetch(),
+//   new Promise((resolve) => {
+//     loadCart(() => {
+//       resolve();
+//     });
+//   }),
+// ]).then(() => {
+//   renderOrderSummary();
+//   renderPaymentSummary();
+// });
 
 
 
-
-
+//same thing using async await
+async function loadPage() {
+  await loadProductsFetch();
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value 3');
+    });
+  });
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
 
 // Promise.all([
 //   new Promise((resolve) => {
@@ -37,10 +46,10 @@ Promise.all([
 //     });
 //   })
 // ]).then(() => {
-    
+
 //         renderOrderSummary();
 //         renderPaymentSummary();
-      
+
 // })
 
 new Promise((resolve) => {
@@ -48,14 +57,9 @@ new Promise((resolve) => {
     resolve();
   });
 }).then(() => {
-      renderOrderSummary();
-      renderPaymentSummary();
-    
-})
-
-
-
-
+  renderOrderSummary();
+  renderPaymentSummary();
+});
 
 // loadProducts(() => {
 //   loadCart(() => {
